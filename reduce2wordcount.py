ad9cfa37275249cfa37e61da57fd1fdc16701893
Maokung <50013555+Maokung@users.noreply.github.com>
2019-06-13 22:45:10 +0700

    Update reduce2wordcount.py

--- reduce2wordcount.py
+++ reduce2wordcount.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     WC = dict()
     for line in sys.stdin:
         line = line.strip()
-        word, count = line.split(\t',1)
+        word, count = line.split('\t',1)
         count = int(count)
 
         if word in WC:
